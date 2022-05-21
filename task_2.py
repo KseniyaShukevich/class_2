9c7aff6f84f071bff2720d733869f781721426e4
kseniya shukevich <kseniya.shukevich@gmail.com>
2022-05-21 14:46:34 +0300

    Fix names of variables

--- task_2.py
+++ task_2.py
@@ -1,9 +1,9 @@
-userString = input('Введите значения, разделяя одним пробелом: ')
+user_string = input('Введите значения, разделяя одним пробелом: ')
 
-newList = userString.split()
+new_list = user_string.split()
 
-for i in range(0, len(newList), 2):
-    if i + 1 < len(newList):
-        newList[i], newList[i + 1] = newList[i + 1], newList[i]
+for i in range(0, len(new_list), 2):
+    if i + 1 < len(new_list):
+        new_list[i], new_list[i + 1] = new_list[i + 1], new_list[i]
 
-print('Результат:', newList)
+print('Результат:', new_list)
